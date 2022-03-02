7f12e053ee162ae7a1fddf8d4c168b3c1eb8544e
purvatraders <95539166+purvatraders@users.noreply.github.com>
2022-03-02 15:02:44 +0530

    bb

---
+++ wp-content/plugins/seo-by-rank-math-pro/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '047b7ae6cd6f3690856a0fd70261c1dd331427c7',
+    'name' => 'mythemeshop/rank-math-pro',
+  ),
+  'versions' => 
+  array (
+    'mythemeshop/rank-math-pro' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '047b7ae6cd6f3690856a0fd70261c1dd331427c7',
+    ),
+  ),
+);
